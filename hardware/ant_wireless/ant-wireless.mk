bf8461e91ceb6a1af411d1baef81026b2a2d89dd
Maurits van der Hoorn <vanderhoornm@gmail.com>
2012-05-15 17:42:40 -0600

    Added back in the dummy library
    +com.dsi.ant.antradio_library.jar

--- hardware/ant_wireless/ant-wireless.mk
+++ hardware/ant_wireless/ant-wireless.mk
@@ -1,6 +1,10 @@
 PRODUCT_PACKAGES += \
+        com.dsi.ant.antradio_library \
         AntHalService \
         libantradio \
         ANTRadioService \
         antradio_app \
+
+PRODUCT_COPY_FILES += \
+        hardware/ant_wireless/antradio_library/com.dsi.ant.antradio_library.xml:system/etc/permissions/com.dsi.ant.antradio_library.xml \
 
